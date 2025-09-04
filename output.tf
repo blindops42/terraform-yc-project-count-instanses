8816b5486cdd4b94c590e6c256a87ef3745be526
Sergey Semenov <blindops42@gmail.com>
2025-09-05 01:10:17 +0200

    Edit output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "ansible_inventory_file" {
-  value = join("\n", [for i in range(var.vm_count) : "server-${i + 1} ansible_host=${yandex_compute_instance.vm[i].network_interface[0].nat_ip_address} ansible_user=ubuntu"])
+  value = join("\n", [for i in range(var.vm_count) : server-${i + 1} ansible_host=${yandex_compute_instance.vm[i].network_interface[0].nat_ip_address} ansible_user=ubuntu])
 }
