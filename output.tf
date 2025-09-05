615536e4498bb1ad6996767939262c3a98e82835
Sergey Semenov <blindops42@gmail.com>
2025-09-05 13:31:52 +0200

    Edit output.tf ansible_ips

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ansible_inventory_file" {
   value = join("\n", [for i in range(var.vm_count) : "server-${i + 1} ansible_host=${yandex_compute_instance.vm[i].network_interface[0].nat_ip_address} ansible_user=ubuntu"])
 }
+output "ansible_ips" {
+  description = "Список публичных IP для Ansible"
+  value       = [for vm in yandex_compute_instance.vm : vm.network_interface[0].nat_ip_address]
+}
