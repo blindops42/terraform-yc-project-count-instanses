2160dd2548fafdc1c4312c10be1da125c0a1778c
Sergey Semenov <blindops42@gmail.com>
2025-09-06 00:04:34 +0200

    Edit output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "yc_ansible_inventory" {
-  value = join("\n", [for i in range(var.vm_count) : "server-${i + 1} ansible_host=${yandex_compute_instance.vm[i].network_interface[0].nat_ip_address} ansible_user=ubuntu"])
+  value = join("\n", [for i in range(var.vm_count) : "yc-${i + 1} ansible_host=${yandex_compute_instance.vm[i].network_interface[0].nat_ip_address} ansible_user=ubuntu"])
 }
